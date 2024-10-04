4125a10df596c92f012e2b77e5ff829c45eb56e7
AverageFrancis <142629365+AverageFrancis@users.noreply.github.com>
2024-10-04 09:09:03 -0300

    a

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let estados = ['AC','AL','AP','AM','BA','CE','ES','GO','MA','MT','MS','MG','PA',
 
 
 let i = 0;
-while(i<27){
+while(i<estados.length){
 console.log(i);
 console.log(estados[i])
 i++;
