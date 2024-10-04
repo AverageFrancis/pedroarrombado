aecdc44a437502569c85423528671f3687bf42dc
AverageFrancis <142629365+AverageFrancis@users.noreply.github.com>
2024-10-04 09:20:30 -0300

    a

--- main.js
+++ main.js
@@ -10,6 +10,6 @@ let estados = ['AC','AL','AP','AM','BA','CE','ES','GO','MA','MT','MS','MG','PA',
 
 // }
 for (let index = 0; index < estados.length; index++) {
-    const element = array[index];
-    console.log(index)
+    const element(j);
+    console.log(estados[j]);
 }
