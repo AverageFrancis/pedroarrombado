e6114d31037f3fc6f378d64089c9be50dd4783aa
AverageFrancis <142629365+AverageFrancis@users.noreply.github.com>
2024-10-04 08:53:56 -0300

    a

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 let estados = ['AC','AL','AP','AM','BA','CE','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO','DF']
+
+
+let i = 0;
+while(i<10){
+console.log(1);
+i = i + 1;
+
+
+}
