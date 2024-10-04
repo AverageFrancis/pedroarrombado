7bfa244c785283879543d5a5d7e330fc4b65d482
AverageFrancis <142629365+AverageFrancis@users.noreply.github.com>
2024-10-04 08:41:53 -0300

    a

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-let estados = [AC,'Al','AP','AM','BA','CE','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO','DF']
+let estados = [AC,'AL','AP','AM','BA','CE','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO','DF']
