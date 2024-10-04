1ad8a5332e7003c56f3e216acd92ff315ba05eab
AverageFrancis <142629365+AverageFrancis@users.noreply.github.com>
2024-10-04 09:18:38 -0300

    a

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 let estados = ['AC','AL','AP','AM','BA','CE','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO','DF']
 
 
-let i = 0;
-while(i<estados.length){
-console.log(i);
-console.log(estados[i])
-i++;
+// let i = 0;
+// while(i<estados.length){
+// console.log(i);
+// console.log(estados[i])
+// i++;
 
 
+// }
+for (let index = 0; index < estados.length; index++) {
+    const element = array[index];
+    console.log(index)
 }
